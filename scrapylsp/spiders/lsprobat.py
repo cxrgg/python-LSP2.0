f49e2767ea449a2aac23002f06b5c0ebb361d068
cxr_work <cxr@unisolution.cn>
2020-11-26 17:15:26 +0800

    优化进入下一页爬取

--- scrapylsp/spiders/lsprobat.py
+++ scrapylsp/spiders/lsprobat.py
@@ -12,19 +12,25 @@ class LsprobatSpider(scrapy.Spider):
     # start_urls = ['https://www.iteye.com/blog/justcoding-2086233/']
 
     allowed_domains = ['qql6k.com:5561']
-    start_urls = ['https://qql6k.com:5561/luyilu/list_5_{:d}.html/'.format(i) for i in range(1, 100)]
+    start_urls = ['https://qqc6k.com:5561/luyilu/']
 
     def parse(self, response):
         base_url = 'https://qql6k.com:5561'
-        base_path = "../"
+        base_path = "../pic/"
         img_paths = response.xpath('//h2/a/@href').extract()
         names = response.xpath('//h2/a/@title').extract()
+        # print("*"*200)
+        # print(names)
         for img_path, name in zip(img_paths, names):
             item = ScrapylspItem()
             item["name"] = name
             mkdir(base_path + name)
             scrapy.Request(base_url + img_path, callback=self.parse_img(path=name))
-            yield item
+        next_path = response.xpath('//div[2]/ul/li[6]/a/@href').extract_first()
+        print()
+        if next_path != "javascript:;":
+            next_path = "https://qqc6k.com:5561/luyilu/" + next_path
+            scrapy.Request(next_path, callback=self.parse())
 
     def parse_img(self, response, path):
         pass
