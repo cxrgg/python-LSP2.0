2c7ef5d7cab82342345ead47f406a304a1ce8c5c
cxr_work <cxr@unisolution.cn>
2020-11-28 00:33:24 +0800

    优化图集下载完成提示

--- scrapylsp/spiders/lsprobat.py
+++ scrapylsp/spiders/lsprobat.py
@@ -18,7 +18,6 @@ class LsprobatSpider(scrapy.Spider):
         for img_path, name in zip(img_paths, names):
             # 这一图级的名字(一定要在这传,后面麻烦)
             name = subBrackets(name)
-            # mkdir(IMAGES_STORE + name)
             yield scrapy.Request(base_url + img_path, callback=self.parse_img, meta={'name': name})
 
         # 文件列表下一页自动跳转
@@ -32,8 +31,8 @@ class LsprobatSpider(scrapy.Spider):
     def parse_img(self, response):
         # 先判断是否只有一张图片
         next_img_paths = response.xpath("//div[2]/ul/li/a/@href").extract()
-        isClass = response.xpath("//div[2]/ul/li/a/@class").extract()
-        if next_img_paths or isClass:
+        buttonNames = response.xpath("//div[2]/ul/li/a/text()").extract()
+        if next_img_paths:
             # 处理图片页面:获取文件夹名称和图片下载链接
             imgUrls = response.xpath("//div/article/p/img/@src").extract()
             # 当前图片目录名
@@ -47,7 +46,7 @@ class LsprobatSpider(scrapy.Spider):
             pic_prefix_next = subUrlBefore(response.request.url)
 
             # 图片页面下一页,自动跳转
-            if next_img_paths:
+            if buttonNames[-1] == '下一页':
                 next_img_path = pic_prefix_next + next_img_paths[-1]
                 yield scrapy.Request(next_img_path, callback=self.parse_img, meta={'name': name})
             else:
